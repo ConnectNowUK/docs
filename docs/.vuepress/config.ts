704d6567c0ab42aa04cdabac3c2f1355deaf82db
akr4m <akram@futurestation.net>
2024-01-20 21:13:54 +0600

    Update ConnectNow Docs configuration

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,142 +6,142 @@ import { searchPlugin } from "@vuepress/plugin-search";
 import { path } from "@vuepress/utils";
 
 export default defineUserConfig({
-    lang: "en-GB",
-    title: "ConnectNow Docs",
-    description: "Documentation for ConnectNow services",
+  lang: "en-GB",
+  title: "ConnectNow Docs",
+  description: "Documentation for ConnectNow services",
 
-    theme: defaultTheme({
-        logo: "https://d1jdn0pmurlmlu.cloudfront.net/cn-logo.svg",
-        docsRepo: "ConnectNowUK/docs",
-        docsDir: "docs",
-        navbar: [
-            // {
-            //     text: 'Home',
-            //     link: '/',
-            // },
+  theme: defaultTheme({
+    logo:
+      "https://images.squarespace-cdn.com/content/v1/5f50f8731ff54a132283757b/4d363c79-a765-45fa-a0e8-b08d95799bea/cn+logo.png",
+    docsRepo: "ConnectNowUK/docs",
+    docsDir: "docs",
+    navbar: [
+      // {
+      //     text: 'Home',
+      //     link: '/',
+      // },
 
-            // {
-            //     text: 'Issue Tracker',
-            //     children: ['/issue-tracker/user-guide/', '/issue-tracker/developer-guide/api/'],
-            // },
+      // {
+      //     text: 'Issue Tracker',
+      //     children: ['/issue-tracker/user-guide/', '/issue-tracker/developer-guide/api/'],
+      // },
 
-            {
-                text: "General",
-                children: [
-                    {
-                        text: "Technical",
-                        children: [
-                            "/general/api-philosophy.html",
-                            "/general/architecture.html",
-                            "/general/device-support.html",
-                        ],
-                    },
-                    {
-                        text: "Reference",
-                        children: [
-                            "/general/integration.html",
-                            "/general/flow-builder.html",
-                        ],
-                    },
-                ],
-            },
-
-            {
-                text: "Issue Tracker",
-                children: [
-                    {
-                        text: "User Guides",
-                        children: ["/issue-tracker/user-guide/"],
-                    },
-                    {
-                        text: "Developer Guides",
-                        children: ["/issue-tracker/developer-guide/api/"],
-                    },
-                ],
-            },
-
-            {
-                text: "Chat",
-                children: [
-                    {
-                        text: "User Guides",
-                        children: [
-                            "/chat/user-guide/user-guide.md",
-                            {
-                                text: "Guide for Company Managers",
-                                link:
-                                    "/chat/user-guide/company-administration-guide.md",
-                            },
-                        ],
-                    },
-                    {
-                        text: "Developer Guides",
-                        children: ["/chat/developer-guide/api/"],
-                    },
-                ],
-            },
-
-            {
-                text: "Queue",
-                children: [
-                    {
-                        text: "User Guides",
-                        children: [
-                            "/queue-system/user-guide/concept-overview.md",
-                            "/queue-system/user-guide/user-guide.md",
-                        ],
-                    },
-                    {
-                        text: "Developer Guides",
-                        children: ["/queue-system/developer-guide/api/"],
-                    },
-                ],
-            },
-
-            {
-                text: "Policies",
-                children: [
-                    "/policies/data-policies.html",
-                    "/policies/terms.html",
-                    "/policies/privacy.html",
-                    "/policies/cookies.html",
-                ],
-            },
+      {
+        text: "General",
+        children: [
+          {
+            text: "Technical",
+            children: [
+              "/general/api-philosophy.html",
+              "/general/architecture.html",
+              "/general/device-support.html",
+            ],
+          },
+          {
+            text: "Reference",
+            children: [
+              "/general/integration.html",
+              "/general/flow-builder.html",
+            ],
+          },
         ],
-    }),
-    plugins: [
-        googleAnalyticsPlugin({
-            id: "G-HMS6SBZGCE",
-        }),
-        searchPlugin({
-            locales: {
-                "/": {
-                    placeholder: "Search",
-                },
-                "/zh/": {
-                    placeholder: "搜索",
-                },
-            },
-        }),
-        registerComponentsPlugin({
-            componentsDir: path.resolve(__dirname, "./components"),
-        }),
-        containerPlugin({
-            type: "authorization",
-            locales: {
-                "/": {
-                    defaultInfo: "Authorization required",
-                },
-                "/zh/": {
-                    defaultInfo: "提示",
-                },
-            },
-            before: (info: string): string =>
-                `<div class="custom-container tip authorization">${
-                    info ? `<p class="custom-container-title">${info}</p>` : ""
-                }`,
-            after: (): string =>
-                '<p><small>All authorization requests require you to first be <router-link to="#authentication">authenticated</router-link>.</small></p></div>\n',
-        }),
+      },
+
+      {
+        text: "Issue Tracker",
+        children: [
+          {
+            text: "User Guides",
+            children: ["/issue-tracker/user-guide/"],
+          },
+          {
+            text: "Developer Guides",
+            children: ["/issue-tracker/developer-guide/api/"],
+          },
+        ],
+      },
+
+      {
+        text: "Chat",
+        children: [
+          {
+            text: "User Guides",
+            children: [
+              "/chat/user-guide/user-guide.md",
+              {
+                text: "Guide for Company Managers",
+                link: "/chat/user-guide/company-administration-guide.md",
+              },
+            ],
+          },
+          {
+            text: "Developer Guides",
+            children: ["/chat/developer-guide/api/"],
+          },
+        ],
+      },
+
+      {
+        text: "Queue",
+        children: [
+          {
+            text: "User Guides",
+            children: [
+              "/queue-system/user-guide/concept-overview.md",
+              "/queue-system/user-guide/user-guide.md",
+            ],
+          },
+          {
+            text: "Developer Guides",
+            children: ["/queue-system/developer-guide/api/"],
+          },
+        ],
+      },
+
+      {
+        text: "Policies",
+        children: [
+          "/policies/data-policies.html",
+          "/policies/terms.html",
+          "/policies/privacy.html",
+          "/policies/cookies.html",
+        ],
+      },
     ],
+  }),
+  plugins: [
+    googleAnalyticsPlugin({
+      id: "G-HMS6SBZGCE",
+    }),
+    searchPlugin({
+      locales: {
+        "/": {
+          placeholder: "Search",
+        },
+        "/zh/": {
+          placeholder: "搜索",
+        },
+      },
+    }),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, "./components"),
+    }),
+    containerPlugin({
+      type: "authorization",
+      locales: {
+        "/": {
+          defaultInfo: "Authorization required",
+        },
+        "/zh/": {
+          defaultInfo: "提示",
+        },
+      },
+      before: (info: string): string =>
+        `<div class="custom-container tip authorization">${
+          info ? `<p class="custom-container-title">${info}</p>` : ""
+        }`,
+      after: (): string =>
+        '<p><small>All authorization requests require you to first be <router-link to="#authentication">authenticated</router-link>.</small></p></div>\n',
+    }),
+  ],
 });
